744561e6444c85e4f2aa7a12b73718b7cfbb7a55
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-05-05 22:49:39 +0530

    Restructuring and init rock paper scissors

--- commands.js
+++
@@ -1,9 +0,0 @@
-module.exports = [{
-	name: 'hello',
-	description: 'Sends a hello message',
-	response: async (interaction) => interaction.reply(`Hello. Hope to say something more than this soon.`)
-}, {
-	name: 'help',
-	description: 'Returns a help message (coming soon)',
-	response: async (interaction) => interaction.reply('Coming soon...')
-}]
