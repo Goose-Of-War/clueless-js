99025503e368508d951abef6d6c74144f9f7473b
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-05-06 13:07:03 +0530

    try...catch changed a bit

--- commands/rock-paper-scissors/vs_bot.js
+++ commands/rock-paper-scissors/vs_bot.js
@@ -1,12 +1,12 @@
 const gameMethods = require('./base');
 
 module.exports = againstUser = async function (player) {
+	// Create a DM channel if doesn't exist.
+	await player.createDM();
+	// Mesage to confirm game start
+	const startingMessage = await gameMethods.sendStartingMessage(player);
+	// Check for button click response
 	try {
-		// Create a DM channel if doesn't exist.
-		await player.createDM();
-		// Mesage to confirm game start
-		const startingMessage = await gameMethods.sendStartingMessage(player);
-		// Check for button click response
 		const response = await startingMessage.awaitMessageComponent({
 			filter: res => res.user.id === player.id,
 			time: 7_000
@@ -17,7 +17,6 @@ module.exports = againstUser = async function (player) {
 				content: 'Aww, too bad...',
 				components: []
 			});
-			setTimeout(() => startingMessage.delete(), 2_500);
 			return {
 				content: `<@${player.id}> has cancelled the game.`
 			};
@@ -31,13 +30,19 @@ module.exports = againstUser = async function (player) {
 			components: []
 		});
 		setTimeout(() => startingMessage.delete(), 2_500);
-		// Send the message for the user's choice
-		const gameMessage = await gameMethods.sendRockPaperScissorsMessage(player);
-		// Choose one from rock paper scissors for the bot
-		const botResponse = {
-			customId: ['rock', 'paper', 'scissors'][Math.floor(3 * Math.random())]
-		}
-		// Check for button click response
+	} catch (err) {
+		console.log(err);
+		setTimeout(() => startingMessage.delete(), 2_500);
+		throw new Error(err);
+	}
+	// Send the message for the user's choice
+	const gameMessage = await gameMethods.sendRockPaperScissorsMessage(player);
+	// Choose one from rock paper scissors for the bot
+	const botResponse = {
+		customId: ['rock', 'paper', 'scissors'][Math.floor(3 * Math.random())]
+	}
+	// Check for button click response
+	try {
 		const userResponse = await gameMessage.awaitMessageComponent({
 			filter: res => res.user.id === player.id,
 			time: 7_000
@@ -47,16 +52,17 @@ module.exports = againstUser = async function (player) {
 			components: []
 		})
 		setTimeout(() => gameMessage.delete(), 2_500);
-		// Time to see who is the winner
-		const gameOutputCode = gameMethods.determineWinner(userResponse, botResponse);
-		if (!gameOutputCode) return {
-			content: 'The game ended in a draw. Thanks for playing though :)'
-		};
-		return {
-			content: `The winner of this game is... <@${(gameOutputCode === -1 ? client.user : player).id}>`
-		}
 	} catch (err) {
 		console.log(err);
+		setTimeout(() => gameMessage.delete(), 2_500);
 		throw new Error(err);
-	}
+	} 
+	// Time to see who is the winner
+	const gameOutputCode = gameMethods.determineWinner(userResponse, botResponse);
+	if (!gameOutputCode) return {
+		content: 'The game ended in a draw. Thanks for playing though :)'
+	};
+	return {
+		content: `The winner of this game is... <@${(gameOutputCode === -1 ? client.user : player).id}>`
+	};
 }
