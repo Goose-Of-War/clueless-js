03e5bf1104529d43507c2bde229876d750b28e3f
Goose-Win11 <64352864+Goose-Of-War@users.noreply.github.com>
2023-09-30 10:39:07 +0530

    Lint ;-;

--- commands/roll-a-die.js
+++ commands/roll-a-die.js
@@ -5,11 +5,12 @@ module.exports = {
 		.setName('roll-a-die')
 		.setDescription('Rolls a die and returns the value.')
 		.addIntegerOption(option => option
-				.setName('die-value')
-				.setDescription('The maximum value on the die.')
-			),
+			.setName('die-value')
+			.setDescription('The maximum value on the die.')
+		),
 	execute: async (interaction) => {
-		if ((a => a !== null && a < 2)(interaction.options.getInteger('die-value'))) return await interaction.reply(`At least set the value on the die to something greater than 1`);
+		if ((a => a !== null && a < 2)(interaction.options.getInteger('die-value')))
+			return await interaction.reply(`At least set the value on the die to something greater than 1`);
 		const maxValue = interaction.options.getInteger('die-value') || 6;
 		const dieValue = Math.floor(Math.random() * maxValue) + 1;
 		return await interaction.reply(`The die has been rolled. The result is... **${dieValue}**!`);
